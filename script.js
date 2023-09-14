418ae9bb8b2100d37da789462f83228cf426d365
AndrusTassa <143269077+AndrusTassa@users.noreply.github.com>
2023-09-14 21:34:45 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function kikkeliskokkelis() {
+    var inputNumber = document.getElementById("inputNumber").value;
+    var result = Math.PI * inputNumber;
+  
+    document.getElementById("resultLabel").innerHTML = "Pii lähiluku: " + result;
+  }
+
+document.title = "hullu homma";
